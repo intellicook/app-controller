b919bb115168ae2591ed0c90c419924207188ed6
LioQing <s2010157@gmail.com>
2024-08-02 23:31:39 +0800

    Add required attribute

--- IntelliCook.AppController.Api/Models/Health/HealthCheckModel.cs
+++ IntelliCook.AppController.Api/Models/Health/HealthCheckModel.cs
@@ -1,7 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace IntelliCook.AppController.Api.Models.Health;
 
 public class HealthCheckModel
 {
+    [Required]
     public required string Name { get; set; }
+
+    [Required]
     public required HealthStatusModel Status { get; set; }
 }
